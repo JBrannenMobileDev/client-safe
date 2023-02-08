6f7180102dff9633afb0ff5e330483eb56a6c87e
jonathanbrannen <jonathan@dandylight.com>
2023-02-07 17:25:40 -0800

    font changed again!

--- lib/utils/EnvironmentUtil.dart
+++ lib/utils/EnvironmentUtil.dart
@@ -6,7 +6,7 @@ class EnvironmentUtil {
   static const String STAGE = "stage";
   static const String DEV = "dev";
 
-  String _currentEnvironment = DEV;
+  String _currentEnvironment = PROD;
 
   factory EnvironmentUtil() {
     return _environmentUtil;
